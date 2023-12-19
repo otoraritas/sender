0ea98df15efd22e771f498352e5ce3e21c30029a
otoraritas <bisnsisdc401@gmail.com>
2023-12-19 14:19:57 +0700

    Bebas

---
+++ success.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Success</title>
+    <style>
+        body {
+            background-color: #111;
+            color: #fff;
+            font-family: 'Arial', sans-serif;
+            text-align: center;
+            margin: 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        p {
+            font-size: 1.5rem;
+        }
+
+        a {
+            color: #00ffcc;
+            text-decoration: none;
+            border-bottom: 2px solid #00ffcc;
+        }
+
+        a:hover {
+            color: #fff;
+            border-bottom: 2px solid #fff;
+        }
+
+        h1 {
+            font-size: 3rem;
+            margin-top: 20px;
+            text-shadow: 0 0 10px #00ffcc, 0 0 20px #00ffcc, 0 0 40px #00ffcc;
+        }
+    </style>
+</head>
+<body>
+    <h1>Welcome</h1>
+    <p>Ingin Mengirim Pesan Lagi? <a href="index.html">Click Here</a>.</p>
+</body>
+</html>
+
